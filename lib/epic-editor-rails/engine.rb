16d314d4c9bd72a0500b8db23fc6d58477bf0705
Shane McDonald <me@shanemcd.com>
2014-01-21 14:18:46 -0500

    Add initializer block to engine that precompiles assets. Necessary for Heroku.

--- lib/epic-editor-rails/engine.rb
+++ lib/epic-editor-rails/engine.rb
@@ -2,6 +2,15 @@ module Epic
   module Editor
     module Rails
       class Engine < ::Rails::Engine
+        if ::Rails.version >= "3.1"
+          initializer "Precompile EpicEditor assets" do |app|
+            app.config.assets.precompile += [
+              "epiceditor.js", "base/epiceditor.css",
+              "editor/epic-dark.css", "editor/epic-light.css",
+              "preview/bartik.css", "preview/github.css", "preview-dark.css"
+            ]
+          end
+        end
       end
     end
   end
